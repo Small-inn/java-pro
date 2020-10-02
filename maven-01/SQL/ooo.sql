22dd1114c6f2ef54612ddc15bb3653ecafc7931e
liuxie <zyhd.liuxie@waibao.cntaiping.com>
2020-10-02 17:11:12 +0800

    ....

--- maven-01/SQL/ooo.sql
+++ maven-01/SQL/ooo.sql
@@ -17,3 +17,24 @@ comment on column T_LX_PRODUCT_CONFIG.PRODUCT_NAME  is '险种名称';
 comment on column T_LX_PRODUCT_CONFIG.SEND_START_TIME  is '开始推送时间';
 comment on column T_LX_PRODUCT_CONFIG.SAVE_START_TIME  is '开始保存时间';
 comment on column T_LX_PRODUCT_CONFIG.SOURCE_TYPE  is '来源类型 1：MQ';
+
+
+create sequence SEQ_product_config
+increment by 1
+start with 1
+minvalue 1
+maxvalue 999999999999
+order
+cache 20
+cycle;
+
+
+SELECT * FROM ( SELECT ROWNUM rowno, p.* FROM T_LX_PRODUCT_CONFIG P WHERE PRODUCT_ID = '1' AND PRODUCT_NAME = '1' AND ROWNUM <= 10 ) t WHERE t.rowno >= 1
+
+
+
+
+INSERT INTO T_LX_PRODUCT_CONFIG
+(ID, PRODUCT_ID, PRODUCT_NAME, SEND_START_TIME, SAVE_START_TIME, SOURCE_TYPE)
+VALUES
+(2, 12, '发的说法', '292012', '21323213', 1)
